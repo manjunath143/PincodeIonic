8e3a4d66e3bb4d93eec1018a975e212c35ebcc6b
Vittal Pai <vittalpai@ymail.com>
2018-07-02 16:55:51 +0530

    Add resource request api to call java adapter

---
+++ src/pages/home/home.ts
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2018 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, NgZone } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  balance: string;
+
+  constructor(public navCtrl: NavController, private zone: NgZone) { 
+    console.log('--> HomePage constructor')
+  }
+
+  getBalance() {
+    var resourceRequest = new WLResourceRequest("/adapters/ResourceAdapter/balance",WLResourceRequest.GET);
+    resourceRequest.send().then((response) => {
+      console.log('-->  getBalance(): Success ', response);
+      this.zone.run(() => {
+        this.balance = response.responseText;
+      });
+       
+    },
+    function(error){
+        console.log('-->  getBalance():  ERROR ', error.responseText);
+        this.zone.run(() => {
+          this.balance = error;
+        });
+    });
+  }
+}
